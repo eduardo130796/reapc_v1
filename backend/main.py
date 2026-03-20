22f4aadebf627ffaec10d427fff02fa2a34772ee
eduardo130796 <82067640+eduardo130796@users.noreply.github.com>
2026-03-20 18:10:57 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes.auth import router as auth_router
+from backend.app.routes.auth import router as auth_router
 
 
 load_dotenv()
